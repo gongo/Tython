4b30737c36a7437a506cdc124db66719951828d9
gongo <gonngo@gmail.com>
2011-04-22 01:51:09 +0900

    ひとまず動いた記念 commit

--- src/AbstractRenderer.cc
+++ src/AbstractRenderer.cc
@@ -2,28 +2,6 @@
 #include "AbstractRenderer.h"
 #include "util.h"
 
-//頂点座標
-float vertices[][2] =
-{
-    {0, 400},
-    {0, 0},
-    {520, 0},
-    {520,  400},
-    // {-0.5f,  0.5f},
-    // {-0.5f, -0.5f},
-    // {0.5f, -0.5f},
-    // {0.5f,  0.5f},
-};
- 
-//uv(st)座標
-float uvs[][2] =
-{
-   {0.f, 0.5f},
-   {0.f, 0.f},
-   {0.5f, 0.f},
-   {0.5f, 0.5f},
-};
-
 unsigned int getClosestPowerOfTwo(unsigned int n)
 {
 	unsigned int m = 2;
@@ -51,14 +29,10 @@ AbstractRenderer::AbstractRenderer(xn::Context *ctx, User* _user) : user(_user)
     depthBuf = new Vector[texWidth*texHeight];
     sceneBuf = new unsigned char[texWidth*texHeight*4];
 
-    // texcoords[0] = (float)width/(float)texWidth;
-    // texcoords[2] = (float)width/(float)texWidth;
-    // texcoords[1] = (float)height/(float)texHeight;
-    // texcoords[7] = (float)height/(float)texHeight;
-    texcoords[0] = (float)texWidth;
-    texcoords[2] = (float)texWidth;
-    texcoords[1] = (float)texHeight;
-    texcoords[7] = (float)texHeight;
+    texcoords[0] = (float)width/(float)texWidth;
+    texcoords[2] = (float)width/(float)texWidth;
+    texcoords[1] = (float)height/(float)texHeight;
+    texcoords[7] = (float)height/(float)texHeight;
 
     texID = 0;
     glGenTextures(1, &texID);
@@ -89,15 +63,15 @@ void AbstractRenderer::draw(void)
     for (int y = 0; y < depthMeta->YRes(); y++) {
         for (int x = 0; x < depthMeta->XRes(); x++, label++) {
             if (*label != 0) {
-                vp[0] = 100;
+                vp[0] = 128;
                 vp[1] = 255;
-                vp[2] = 0;
+                vp[2] = 128;
                 vp[3] = 100;
             } else {
-                vp[0] = 255;
-                vp[1] = 128;
-                vp[2] = 210;
-                vp[3] = 0xff;
+                vp[0] = 0;
+                vp[1] = 0;
+                vp[2] = 0;
+                vp[3] = 0xaa;
             }
             vp += 4;
         }
@@ -105,37 +79,26 @@ void AbstractRenderer::draw(void)
     }
 
     glEnable(GL_TEXTURE_2D);
-    //glColor4f(0.9,0.4,0.75,1);
-    //glEnableClientState(GL_VERTEX_ARRAY);
-    //glEnableClientState(GL_COLOR_ARRAY);
-    //glEnableClientState(GL_TEXTURE_COORD_ARRAY);
     
     glBindTexture(GL_TEXTURE_2D, texID);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, sceneBuf);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
     
-   // //補完フィルタ設定。市松模様なのでnearestでおｋ
+    //補完フィルタ設定。市松模様なのでnearestでおｋ
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    
-    // GLfloat verts[8] = {width - 20, height - 20, width - 20, 0, 0, 0, 0, height - 20};
-   glBegin(GL_QUADS);
-   {
-      glVertex2fv(vertices[0]); glTexCoord2fv(uvs[0]);
-      glVertex2fv(vertices[1]); glTexCoord2fv(uvs[1]);
-      glVertex2fv(vertices[2]); glTexCoord2fv(uvs[2]);
-      glVertex2fv(vertices[3]); glTexCoord2fv(uvs[3]);
-   }
- 
-   glEnd();
 
-    // glTexCoordPointer(2, GL_FLOAT, 0, texcoords);
-    // glVertexPointer(2, GL_FLOAT, 0, verts);
-    // glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-    // glBindTexture(GL_TEXTURE_2D, 0);        
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
-    // //glDisableClientState(GL_VERTEX_ARRAY);
-    // //glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-    //glDisable(GL_TEXTURE_2D);
+    GLfloat verts[8] = {width, height, width, 0, 0, 0, 0, height};
+    glTexCoordPointer(2, GL_FLOAT, 0, texcoords);
+    glVertexPointer(2, GL_FLOAT, 0, verts);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+    glBindTexture(GL_TEXTURE_2D, 0);        
+
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisable(GL_TEXTURE_2D);
 }

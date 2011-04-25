17b95c5de58473a893a1d962ac954be39b444a9f
gongo <gonngo@gmail.com>
2011-04-25 22:33:42 +0900

    printf 消し忘れてた

--- src/Detector/RightStraightTriggerDetector.cc
+++ src/Detector/RightStraightTriggerDetector.cc
@@ -19,7 +19,7 @@ bool RightStraightTriggerDetector::isStand(void)
     Vector hand     = user->positionRightHand();
     Vector forearm  = user->skeletonRightForearm();
     Vector upperArm = user->skeletonRightUpperArm();
-    printf("%f\n", upperArm.dot(forearm));
+
     return (shoulder.Y < hand.Y) && (hand.Y > elbow.Y)
         && upperArm.withinAngle(forearm, 80.0f);
 }
